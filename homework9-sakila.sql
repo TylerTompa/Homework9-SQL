584faedb7fc4240a348a3e7253757cfdf34ad997
Tyler Tompa <tylertompa@email.com>
2018-10-24 23:44:17 -0400

    completed problem 4

--- homework9-sakila.sql
+++ homework9-sakila.sql
@@ -40,3 +40,40 @@ WHERE country IN (
     'China'
     )
 ;
+
+/*
+Section 3
+*/
+
+--3a.
+ALTER TABLE actor
+ADD description BLOB;
+
+--3b.
+ALTER TABLE actor
+DROP COLUMN description;
+
+/*
+Section 4.
+*/
+
+--4a.
+SELECT last_name, COUNT(last_name)
+FROM actor
+GROUP BY 1;
+
+--4b.
+SELECT last_name, COUNT(last_name)
+FROM actor
+GROUP BY 1
+HAVING COUNT(last_name) > 1;
+
+--4c.
+UPDATE actor
+SET first_name = 'HARPO'
+WHERE actor_id = 172;
+
+--4d.
+UPDATE actor
+SET first_name = 'GROUCHO'
+WHERE first_name = 'HARPO';
