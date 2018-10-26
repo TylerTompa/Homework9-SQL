e3afd0b324cb42e58021ef176c43ddc012b65d42
Tyler Tompa <tylertompa@email.com>
2018-10-26 14:36:05 -0400

    commented through problem 2

--- homework9-sakila.sql
+++ homework9-sakila.sql
@@ -1,13 +1,21 @@
 /*
-Secton 1.
+Secton 1
 */
 
+-- We first run the USE command so that we don't have to specify the database in every query we subsequently run.
+USE sakila
+
 -- 1a.
+-- This returns the first_name and last_name field of every record from the table actor.
 SELECT first_name, last_name
 FROM actor;
 
 -- 1b.
-SELECT CONCAT(first_name, " ", last_name)
+/*
+We use the CONCAT command to display multiple fields as one.  Here we concatenate the values of the first_name field with the values of the last_name field, in order to display the full name as one field.
+We also wrap the concatenation in an UPPER command, to ensure the query is returned in uppercase.
+*/
+SELECT UPPER(CONCAT(first_name, " ", last_name))
 AS 'Actor Name'
 FROM actor;
 
@@ -16,22 +24,30 @@ Section 2
 */
 
 -- 2a.
+-- This selects the actor_id, first_name, and last_name field, from the table actor, only from records where the first_name field is equal to "Joe"
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE first_name = 'Joe';
 
 -- 2b.
+/*
+This returns the actor_id, first_name, and last_name field, from the table actor, only from records where "GEN" appears anywhere in the last_name field.
+*/
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE last_name LIKE '%GEN%';
 
 -- 2c.
+/*
+This selects the actor_id, first_name, and last_name field, from the table actor, only from records where "LI" appears anywhere in the last_name field.  Furthermore, we order the results in alphabetical order by the values of the third field selected- or the last_name field, and additionally we order by the values of the second field selected- or the first_name field.
+*/
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE last_name LIKE '%LI%'
 ORDER BY 3, 2;
 
 -- 2d.
+-- This selects the country_id, and the country field, from the table country, only from records where the value of the country field is equal to any of the following values: Afghanistan, Bangladesh, China.
 SELECT country_id, country
 FROM country
 WHERE country IN (
@@ -54,7 +70,7 @@ ALTER TABLE actor
 DROP COLUMN description;
 
 /*
-Section 4.
+Section 4
 */
 
 --4a.
@@ -77,3 +93,10 @@ WHERE actor_id = 172;
 UPDATE actor
 SET first_name = 'GROUCHO'
 WHERE first_name = 'HARPO';
+
+/*
+Section 5
+*/
+-- 5
+-- The SHOW CREATE TABLE statement is used to show the exact code that produced a table.
+SHOW CREATE TABLE actor;
